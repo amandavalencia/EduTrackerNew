7622d3543d6bba30f2eace34d822fc0add83c3e7
amandavalencia <amandaavalencia@hotmail.com>
2025-12-21 22:17:54 +0100

    Add HelperMethods

---
+++ HelperMethods.cs
@@ -0,0 +1,40 @@
+﻿using EduTrackerNew.Data;
+
+namespace EduTrackerNew
+{
+    internal class HelperMethods
+    {
+        public static int ChoosePosition()
+        {
+            using var context = new EduTrackerDbContext();
+
+            var positions = context.Positions.ToList();
+
+            Console.WriteLine("Choose a position:");
+
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {positions[i].PositionName}");
+            }
+
+            int choice;
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out choice) &&
+                    choice >= 1 &&
+                    choice <= positions.Count)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid choice, try again.");
+            }
+
+            return positions[choice - 1].PositionId;
+        }
+
+    }
+}
